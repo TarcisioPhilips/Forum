ca099fa0ed5de1db7db93f70a16629b7157085e0
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-13 17:57:57 -0300

    feat: header config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import {Post} from "./Post"
-
+import { Header } from "./components/Header"
+import './styles.css'
 export function App() {
   return (
     <div>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Header/>
+
+      <Post 
+        author="Tarcisio Philips" 
+        content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum, laboriosam nostrum voluptate praesentium quae necessitatibus eum ipsum natus tenetur commodi ullam eligendi rerum inventore accusamus deleniti provident debitis facilis!" 
+      />
+      <Post 
+        author="Tarcisio gameplay" 
+        content= "Novo Post" 
+      />
     </div>
   )
 }
