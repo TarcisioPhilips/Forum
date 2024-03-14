3d33a5495f13e25704aaeb47d6c7d86600173e46
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-14 20:30:37 -0300

    feat: creating posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import {Post} from "./Post"
+import {Post} from "./components/Post"
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import './global.css'
 import styles from './App.module.css'
-import { Sidebar } from "./components/Sidebar"
 
 export function App() {
   return (
@@ -12,14 +12,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar/>
         <main>
-          <Post 
-            author="Tarcisio Philips" 
-            content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum, laboriosam nostrum voluptate praesentium quae necessitatibus eum ipsum natus tenetur commodi ullam eligendi rerum inventore accusamus deleniti provident debitis facilis!" 
-          />
-          <Post 
-            author="Tarcisio gameplay" 
-            content= "Novo Post" 
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
