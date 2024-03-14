a57c483e7b64fac77cd93f80ec094e758f60cfaa
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-13 22:00:54 -0300

    feat: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import {Post} from "./Post"
 import { Header } from "./components/Header"
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+
 export function App() {
   return (
     <div>
       <Header/>
 
-      <Post 
-        author="Tarcisio Philips" 
-        content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum, laboriosam nostrum voluptate praesentium quae necessitatibus eum ipsum natus tenetur commodi ullam eligendi rerum inventore accusamus deleniti provident debitis facilis!" 
-      />
-      <Post 
-        author="Tarcisio gameplay" 
-        content= "Novo Post" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            author="Tarcisio Philips" 
+            content= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe earum, laboriosam nostrum voluptate praesentium quae necessitatibus eum ipsum natus tenetur commodi ullam eligendi rerum inventore accusamus deleniti provident debitis facilis!" 
+          />
+          <Post 
+            author="Tarcisio gameplay" 
+            content= "Novo Post" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
