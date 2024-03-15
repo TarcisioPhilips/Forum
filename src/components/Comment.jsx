b4c423edbb4118586e28abee33fd881cb6184b83
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-15 13:58:48 -0300

    feat: comment component and style

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import styles from './Comment.module.css'
+import {Trash, ThumbsUp} from 'phosphor-react'
+
+
+export function Comment(){
+    return(
+        <div className={styles.comment}>
+            <img src='https://github.com/TarcisioPhilips.png'/>
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong> Tarcisio Philips</strong>
+                            <time title="11 de Maio às 08:13" dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={24}/>
+                        </button>
+
+                    </header>
+
+                    <p> Muito bom cara, parabéns!!</p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
