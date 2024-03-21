e2db68504b88eb0076a8f7d3c52acc62b5af582d
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2024-03-20 22:55:52 -0300

    feat: form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,7 +31,12 @@ export function Post({author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value)
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Esse campo é obrigatório!")
     }
 
     function deleteComment(commentToDelete) {
@@ -41,6 +46,8 @@ export function Post({author, publishedAt, content}) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return(
         <article className={styles.post}>
@@ -74,9 +81,12 @@ export function Post({author, publishedAt, content}) {
                     value = {newCommentText}
                     name="comment"
                     placeholder='Deixe seu comentário'
-                    onChange={handleNewCommentChange}/>
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
+                />
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
